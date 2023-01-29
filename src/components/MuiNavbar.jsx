b4a53a500680e002b903a4777a44e048797b40fa
jaseel-koolath <112375897+jaseel-koolath@users.noreply.github.com>
2023-01-29 13:17:44 +0530

    Menu

--- src/components/MuiNavbar.jsx
+++ src/components/MuiNavbar.jsx
@@ -1,8 +1,25 @@
-import { CatchingPokemon } from "@mui/icons-material";
-import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import { CatchingPokemon, KeyboardArrowDown } from "@mui/icons-material";
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { Stack } from "@mui/system";
+import { useState } from "react";
 
 const MuiNavbar = () => {
+  const [anchor, setAnchro] = useState(null);
+  const open = Boolean(anchor);
+  const handleClick = (e) => {
+    setAnchro(e.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchro(null);
+  };
   return (
     <AppBar position='static'>
       <Toolbar>
@@ -16,8 +33,33 @@ const MuiNavbar = () => {
           <Button color='inherit'>Features</Button>
           <Button color='inherit'>Pricing</Button>
           <Button color='inherit'>About</Button>
+          <Button
+            color='inherit'
+            id='resources-button'
+            onClick={handleClick}
+            endIcon={<KeyboardArrowDown />}
+          >
+            Resources
+          </Button>
           <Button color='inherit'>Login</Button>
         </Stack>
+        <Menu
+          id='resources-menu'
+          anchorEl={anchor}
+          open={open}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+        >
+          <MenuItem onClick={handleClose}>Blog</MenuItem>
+          <MenuItem onClick={handleClose}>Podcast</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
